99d33722eafe855fd36e37636723a679245d18d8
sungjoon99 <cs0126j@naver.com>
2023-10-09 20:40:01 +0900

    1

---
+++ index.php
@@ -0,0 +1,21 @@
+<!Doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>WEB></h1>
+        <ol>
+            <li><a href="index.php?id=HTML">HTML</a></li>
+            <li><a href="index.php?id=CSS">CSS</a></li>
+            <li><a href="index.php?id=JavaScript">JavaScript</a></li>
+        </ol>
+        <h2>
+            <?php
+                echo $_GET['id'];
+            ?>
+        </h2>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis neque nemo est, perspiciatis illum officiis, voluptates corporis alias exercitationem magnam rerum necessitatibus, vitae culpa veritatis mollitia temporibus repudiandae inventore dolores.rem
+    </body>
+</html>
