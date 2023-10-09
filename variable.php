ee525b8a0b8cdd67d0fce363d8606b85bef37c5e
sungjoon99 <cs0126j@naver.com>
2023-10-09 20:17:19 +0900

    PHP

---
+++ variable.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+    <body>
+        <h1>Variable</h1>
+        <?php
+        $name = "sungjoon";
+        echo "Lorem ipsum, dolor sit amet consectetur ".$name." adipisicing elit. ".$name."Ut eos possimus labore explicabo ad eveniet ".$name."nulla, et suscipit, consequatur, vero quasi ".$name."dolor odit exercitationem ".$name."illo ab non commodi. Reprehenderit, provident.";
+        ?>
+    </body>
+</html>
